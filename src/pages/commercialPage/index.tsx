83c7802443c7b56192395dac04c4f4fce020466b
artem_nanavov <artemnanavov@gmail.com>
2021-05-03 16:34:04 +0300

    added commercial page

---
+++ src/pages/commercialPage/index.tsx
@@ -0,0 +1,104 @@
+/* eslint-disable max-len */
+import Footer from 'library/common/components/footer';
+import Header from 'library/common/components/header';
+import Reviews from 'library/common/components/reviews';
+import React from 'react';
+import globalStyles from 'resources/styles/globalStyles.scss';
+import image58 from 'resources/images/image58.png';
+import CatalogItem from 'library/common/components/catalogItem';
+import Rectangle144 from 'resources/images/Rectangle144.png';
+import styles from './styles.scss';
+
+const CommercialPage = () => (
+	<>
+		<Header />
+
+		<nav className={styles.commercial}>
+			<div className={styles.commercial__wrap}>
+				<div style={{maxWidth: '808px'}} className={globalStyles.title}>
+					<h1>Commercial Reclaimed Wood Furniture</h1>
+
+					<div />
+				</div>
+
+				<img src={image58} alt="" />
+
+				<h3>Looking To Fill Your Commercial Space With Some Beautiful Reclaimed Wood Furniture?</h3>
+
+				<p>
+					We have built many pieces for commercial spaces and can help you fill a space of any size. If you have your own design in mind, we can help with that too! Simply fill out the form below and we will get back with you right away to discuss your project.
+				</p>
+
+				<div style={{maxWidth: '616px', marginTop: '90px'}} className={globalStyles.title}>
+					<h1>BUY NOW — Trending Furniture</h1>
+
+					<div />
+				</div>
+
+				<div className={styles.commercial__catalog}>
+					<CatalogItem
+						img={Rectangle144}
+						title="Тут будет название товара в 2 строки"
+						description="This Coffee Table with U-Shaped Leg Base is HANDMADE in the USA. It's an ideal..."
+						price="230.00"
+					/>
+
+					<CatalogItem
+						img={Rectangle144}
+						title="Тут будет название товара в 2 строки"
+						description="This Coffee Table with U-Shaped Leg Base is HANDMADE in the USA. It's an ideal..."
+						price="230.00"
+					/>
+
+					<CatalogItem
+						img={Rectangle144}
+						title="Тут будет название товара в 2 строки"
+						description="This Coffee Table with U-Shaped Leg Base is HANDMADE in the USA. It's an ideal..."
+						price="230.00"
+					/>
+
+					<CatalogItem
+						img={Rectangle144}
+						title="Тут будет название товара в 2 строки"
+						description="This Coffee Table with U-Shaped Leg Base is HANDMADE in the USA. It's an ideal..."
+						price="230.00"
+					/>
+
+					<CatalogItem
+						img={Rectangle144}
+						title="Тут будет название товара в 2 строки"
+						description="This Coffee Table with U-Shaped Leg Base is HANDMADE in the USA. It's an ideal..."
+						price="230.00"
+					/>
+
+					<CatalogItem
+						img={Rectangle144}
+						title="Тут будет название товара в 2 строки"
+						description="This Coffee Table with U-Shaped Leg Base is HANDMADE in the USA. It's an ideal..."
+						price="230.00"
+					/>
+
+					<CatalogItem
+						img={Rectangle144}
+						title="Тут будет название товара в 2 строки"
+						description="This Coffee Table with U-Shaped Leg Base is HANDMADE in the USA. It's an ideal..."
+						price="230.00"
+					/>
+
+					<CatalogItem
+						img={Rectangle144}
+						title="Тут будет название товара в 2 строки"
+						description="This Coffee Table with U-Shaped Leg Base is HANDMADE in the USA. It's an ideal..."
+						price="230.00"
+					/>
+				</div>
+			</div>
+		</nav>
+
+		<Reviews />
+
+		<Footer />
+	</>
+);
+
+export default CommercialPage;
--- src/pages/homePage/components/catalog/index.tsx
+++ src/pages/homePage/components/catalog/index.tsx
@@ -2,7 +2,7 @@ import Button from 'library/common/components/ui/button';
 import React from 'react';
 import Rectangle144 from 'resources/images/Rectangle144.png';
 import globalStyles from 'resources/styles/globalStyles.scss';
-import Item from './components/item';
+import CatalogItem from '../../../../library/common/components/catalogItem';
 import styles from './styles.scss';
 
 const Catalog = () => (
@@ -14,56 +14,56 @@ const Catalog = () => (
 		</div>
 
 		<div className={styles.catalog}>
-			<Item
+			<CatalogItem
 				img={Rectangle144}
 				title="Тут будет название товара в 2 строки"
 				description="This Coffee Table with U-Shaped Leg Base is HANDMADE in the USA. It's an ideal..."
 				price="230.00"
 			/>
 
-			<Item
+			<CatalogItem
 				img={Rectangle144}
 				title="Тут будет название товара в 2 строки"
 				description="This Coffee Table with U-Shaped Leg Base is HANDMADE in the USA. It's an ideal..."
 				price="230.00"
 			/>
 
-			<Item
+			<CatalogItem
 				img={Rectangle144}
 				title="Тут будет название товара в 2 строки"
 				description="This Coffee Table with U-Shaped Leg Base is HANDMADE in the USA. It's an ideal..."
 				price="230.00"
 			/>
 
-			<Item
+			<CatalogItem
 				img={Rectangle144}
 				title="Тут будет название товара в 2 строки"
 				description="This Coffee Table with U-Shaped Leg Base is HANDMADE in the USA. It's an ideal..."
 				price="230.00"
 			/>
 
-			<Item
+			<CatalogItem
 				img={Rectangle144}
 				title="Тут будет название товара в 2 строки"
 				description="This Coffee Table with U-Shaped Leg Base is HANDMADE in the USA. It's an ideal..."
 				price="230.00"
 			/>
 
-			<Item
+			<CatalogItem
 				img={Rectangle144}
 				title="Тут будет название товара в 2 строки"
 				description="This Coffee Table with U-Shaped Leg Base is HANDMADE in the USA. It's an ideal..."
 				price="230.00"
 			/>
 
-			<Item
+			<CatalogItem
 				img={Rectangle144}
 				title="Тут будет название товара в 2 строки"
 				description="This Coffee Table with U-Shaped Leg Base is HANDMADE in the USA. It's an ideal..."
 				price="230.00"
 			/>
 
-			<Item
+			<CatalogItem
 				img={Rectangle144}
 				title="Тут будет название товара в 2 строки"
 				description="This Coffee Table with U-Shaped Leg Base is HANDMADE in the USA. It's an ideal..."
