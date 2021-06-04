a68ba0cd649793c255d41f15332b10ccc9039572
artem_nanavov <artemnanavov@gmail.com>
2021-06-04 22:04:15 +0300

    added adaptive for login page

--- src/pages/loginPage/components/form/index.tsx
+++ src/pages/loginPage/components/form/index.tsx
@@ -4,7 +4,13 @@ import React from 'react';
 import styles from './styles.scss';
 
 const regex = /^([A-Z|a-z|0-9](\.|_){0,1})+[A-Z|a-z|0-9]\@([A-Z|a-z|0-9])+((\.){0,1}[A-Z|a-z|0-9]){2}\.[a-z]{2,3}$/gm;
-const mw = {maxWidth: '372px', width: '100%'};
+const mw = {
+	maxWidth: '372px',
+	width: '100%',
+	'@media screen and (maxWidth: 1200px)': {
+		maxWidth: '100%',
+	},
+};
 
 const LoginForm = () => {
 	const [email, setEmail] = React.useState('');
